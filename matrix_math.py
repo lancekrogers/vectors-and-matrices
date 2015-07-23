d67a00314f3df7ea51fdd6646c66688ed666e8a2
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-23 16:17:33 -0400

    only two failures left

--- matrix_math.py
+++ matrix_math.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import numpy as np
+import operator
 
 
 
@@ -21,7 +22,6 @@ def dot(vect_one, vect_two):
         num = vect_one[x] * vect_two[x]
         hammer.append(num)
     return sum(hammer)
-
 
 
 def magnitude(vect):
@@ -54,6 +54,7 @@ def vector_sub(vect_one, vect_two):
         new_vect.append(num)
     return new_vect
 
+
 def vector_sum(*args):
     reduce(shape_check, tuple(args))
     values = [x for x in args]
@@ -62,6 +63,7 @@ def vector_sum(*args):
         num = sum(values[x-1])
         other.append(num)
     return other
+
 
 def vector_multiply(vect, scalar):
     new_vect = [x * scalar for x in vect]
@@ -73,27 +75,34 @@ def vector_mean(*args):
    return (reduce(vector_add, tuple(args))) / len(args)
 
 
-
 def matrix_row(matrix, row):
     return matrix[row]
-
-
 
 
 def matrix_col(matrix, col):
     return [row[col] for row in matrix]
 
 
-
-
-
 def matrix_scalar_multiply(matrix, scalar):
-    pass
+    return [[x * scalar for x in row]for row in matrix]
 
 
-def matrix_vector_multiply(matrix, vector):
-    pass
+
+def matrix_vector_multiply(m, v):
+    shape_check(matrix_row(m, 0), v)
+    rows = len(m)
+    w = [0]*rows
+    irange = range(len(v))
+    sum = 0
+    for j in range(rows):
+        r = m[j]
+        for i in irange:
+            sum += r[i]*v[i]
+        w[j],sum = sum,0
+    return w
 
 
 def matrix_matrix_multiply(matrix_one, matrix_two):
-    pass
+    shape_check(matrix_one, matrix_two)
+
+
